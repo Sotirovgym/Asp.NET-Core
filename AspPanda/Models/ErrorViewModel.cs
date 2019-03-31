0b7c7109a1c1f475155ecf9bcaf217bdee53263e
Georgi Sotirov <sotirovgym@gmail.com>
2019-03-31 17:21:20 +0300

    Created a new project

---
+++ AspPanda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspPanda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
