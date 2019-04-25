6fa63a2c4314e89e7bdab093f10c116de5d5d4e9
Georgi Sotirov <sotirovgym@abv.bg>
2019-04-25 15:12:09 +0300

    Add services project. Adding IPackageRepository and PackageRepository to manage packages in database.

---
+++ Panda/Panda.Services/Interfaces/IPackageRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Panda.Services.Interfaces
+{
+    using Panda.Models.Entities;
+    using System.Collections.Generic;
+
+    public interface IPackageRepository
+    {
+        IEnumerable<Package> GetPackages();
+
+        Package GetPackageById(int id);
+
+        void CreatePackage(Package package);
+    }
+}
---
+++ Panda/Panda.Services/Repositories/PackageRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Panda.Services.Repositories
+{
+    using System.Collections.Generic;
+    using Panda.Models.Entities;
+    using Panda.Services.Interfaces;
+
+    public class PackageRepository : IPackageRepository
+    {
+        public void CreatePackage(Package package)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Package GetPackageById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Package> GetPackages()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
